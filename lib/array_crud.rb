4a53153abcd8e9ac7977e037fde34cee08f6de1d
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-20 07:07:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_start_of_array(array)
   array.shift
 end
 
-def retrieve_element_from_index(array, index_number[2])
-  array.index_number
+def retrieve_element_from_index(array, index_number)
+  array.index_number[2]
 end
 
 def retrieve_first_element_from_array(array)
