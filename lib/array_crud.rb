c31bc03ba46929af6b19c4701bf4cc43753e3bd3
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-20 07:01:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,6 +30,6 @@ def retrieve_first_element_from_array(create_an_array)
   create_an_array.first
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(create_an_array)
+  create_an_array.last
 end
